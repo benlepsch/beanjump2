2787d8dd6e207a0362ed1d44e33d8aecb63e4e5c
Ben Lepsch <blepsch@gmail.com>
2025-02-19 12:30:01 -0600

    this could get interesting

--- game_scripts/player.js
+++ game_scripts/player.js
@@ -7,21 +7,21 @@
 
 class Player {
     constructor(gameObject, canvas, ctx) {
+        this.game = gameObject;
+        this.canvas = canvas;
+        this.ctx = ctx;
+
         this.src = PLAYER_IMG;
         this.width = PLAYER_IMG_WIDTH;
         this.height = PLAYER_IMG_HEIGHT;
-        this.x = 0;
-        this.y = this.game.ground_height - this.height;
 
         this.score = 0;
         this.keys = [];
-
-        this.game = gameObject;
-
-        this.canvas = canvas;
-        this.ctx = ctx;
+        
+        this.x = 0;
+        this.y = this.game.ground_height - this.height;
     }
-
+    
     // checks for player movement
     checkKeys(keyCode) {
         if (!this.game.running) return;
